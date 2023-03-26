fdf3e51a2e9fb8a66e7edf5fd19f905be2cb7ddb
solavrov <2slavrov@gmail.com>
2023-03-26 14:23:57 +0300

    funs deleted

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from movie_pulsar import produce_pulsar, pulsar_params_1, pulsar_params_2
 
 
-produce_pulsar(pulsar_params_2, "pulsar3", True)
+produce_pulsar(pulsar_params_2, "pulsar3")
 
 
--- movie_pulsar.py
+++ movie_pulsar.py
@@ -1,9 +1,10 @@
 from Movie import Movie
 from Frame import Frame
 from math import sin, cos, pi, exp, sqrt
+from cmath import exp as c_exp
 from Clr import Clr
 from numpy import array
-from funs import pulsar
+
 
 pulsar_params_1 = {
 
@@ -150,6 +151,17 @@ pulsar_params_2 = {
     )
 
 }
+
+
+def pulsar(x, y, t, p):
+    z = complex(x, y)
+    d = complex(p["shift"]["x"], p["shift"]["y"])
+    z = z * c_exp(complex(0, 1) * -p["omega"]["center"] * t)
+    z = (z - d) * c_exp(complex(0, 1) * -p["omega"]["self"] * t) + d
+    x = z.real
+    y = z.imag
+    return exp(-(cos(p["omega"]["pulse"] * t + p["phase"]) * (x - p["shift"]["x"]) / p["scale"]["x"]) ** 2 -
+               (sin(p["omega"]["pulse"] * t + p["phase"]) * (y - p["shift"]["y"]) / p["scale"]["y"]) ** 2)
 
 
 def produce_pulsar(params, file_name, do_save_frames=False):
