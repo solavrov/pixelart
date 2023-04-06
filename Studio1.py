aa510f9abb3e48a8acb7849bd8c64821209f5be6
solavrov <2slavrov@gmail.com>
2023-04-06 18:14:47 +0300

    inf

--- Studio1.py
+++ Studio1.py
@@ -8,14 +8,16 @@ from Clr import Clr
 from numpy import array
 from random import randrange as rnd
 from random import choice
+from random import uniform
 
 
 class Root:
 
-    def __init__(self, value, power, rot):
+    def __init__(self, value, power, rotate, phi=0.0):
         self.value = value
         self.power = power
-        self.rot = rot
+        self.rot = rotate
+        self.phi = phi
 
 
 class Studio1:
@@ -23,9 +25,9 @@ class Studio1:
     def __init__(self,
                  w=128,
                  h=128,
-                 times=array(range(64)),
+                 times=array(range(60)),
                  fps=20,
-                 omega=2 * pi / 64,
+                 omega=2 * pi / 60,
                  pow_set=(-3, -2, -1, 1, 2, 3),
                  rot_set=(-1, 1),
                  clr_min=Clr.g,
@@ -46,9 +48,13 @@ class Studio1:
         self.roots: [Root] = []
         self.func = None
 
-    def append_root(self, root, power, rot):
-        self.roots.append(Root(root, power, rot))
+    def append_root(self, root, power, rotate):
+        self.roots.append(Root(root, power, rotate))
         return self
+
+    def set_num_of_frames(self, n):
+        self.times = array(range(n))
+        self.omega = 2 * pi / n
 
     def make_rand_roots(self, n):
         self.roots = []
@@ -84,9 +90,11 @@ class Studio1:
         if self.func is None:
             sys.exit("produce: no fun")
         else:
+            print("producing! please, wait...")
             m = Movie(self.w, self.h, fps=self.fps)
             m.shoot_func(self.func, self.times, clr_min=self.clr_min, clr_max=self.clr_max, clr_mid=self.clr_mid)
             m.write_gif(filename)
+            print("done!")
 
     def produce_rand(self, num_of_roots, filename):
         self.make_rand_roots(num_of_roots)
@@ -101,3 +109,41 @@ class Studio1:
             frame.draw_func(self.func, self.times[frame_number],
                             clr_min=self.clr_min, clr_max=self.clr_max, clr_mid=self.clr_mid)
             frame.write_gif(filename)
+
+    def make_rand_roots_inf(self, n):
+        self.roots = []
+        for _ in range(n):
+            phi = uniform(0, 2 * pi)
+            v = complex(cos(phi), sin(phi) * cos(phi)) * 3 / 4
+            self.roots.append(Root(v, choice(self.pow_set), choice(self.rot_set), phi))
+
+    def rotate_roots_inf(self, t):
+        if len(self.roots) == 0:
+            sys.exit("rotate_roots: no roots")
+        else:
+            rotated_roots = []
+            for r in self.roots:
+                phi = r.phi + r.rot * self.omega * t
+                v = complex(cos(phi), sin(phi) * cos(phi)) * 3 / 4
+                rotated_roots.append(Root(v, r.power, r.rot, phi))
+            return rotated_roots
+
+    def make_func_inf(self):
+        if len(self.roots) == 0:
+            sys.exit("make_func: no roots")
+        else:
+
+            def f(x, y, t):
+                z = complex(x / (self.w / 2), y / (self.h / 2))
+                roots = self.rotate_roots_inf(t)
+                zz = 1
+                for r in roots:
+                    zz *= (z - r.value) ** r.power
+                return abs(arg(zz))
+
+            self.func = f
+
+    def produce_rand_inf(self, num_of_roots, filename):
+        self.make_rand_roots_inf(num_of_roots)
+        self.make_func_inf()
+        self.produce(filename)
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from Clr import Clr
+from math import sin, cos, pi, exp, sqrt, log
+from cmath import exp as c_exp
 from Studio1 import Studio1
 
-
 s = Studio1()
-# s.append_root(0, 2).append_root(-1)
+
+# r1 = 2 / 3 * c_exp(1j * 1 / 6 * 2 * pi)
+# r2 = 2 / 3 * c_exp(1j * 2 / 6 * 2 * pi)
+# r3 = 2 / 3 * c_exp(1j * 3 / 6 * 2 * pi)
+# r4 = 2 / 3 * c_exp(1j * 4 / 6 * 2 * pi)
+# r5 = 2 / 3 * c_exp(1j * 5 / 6 * 2 * pi)
+# r6 = 2 / 3 * c_exp(1j * 6 / 6 * 2 * pi)
+#
+# s.append_root(r1, 1, 2).append_root(r2, 1, 2).append_root(r3, 1, 2)
+# s.append_root(r4, 1, 2).append_root(r5, 1, 2).append_root(r6, 1, 2)
 # s.make_func()
-# s.produce_frame(0, "frame10")
-s.produce_rand(5, "complex3")
+# s.produce_frame(0, "frameX")
+# s.produce("complex8")
 
-
-
-
-
-
-
-
-
-
-
-
+s.produce_rand_inf(10, "complex_inf_1")
 
 
